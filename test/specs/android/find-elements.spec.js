d6f0a665dd40c083c1da9ca5c4bba3f9caded1df
Gagan Keshav <gagankeshav@gmail.com>
2023-06-18 18:47:19 +1200

    Adding test to find elements via UIAutomator functions

--- test/specs/android/find-elements.spec.js
+++ test/specs/android/find-elements.spec.js
@@ -20,7 +20,7 @@ describe('Find elements on Android app', ()=>{
         await expect(clsElement).toHaveText('API Demos')
     })
 
-    it.only('should be able to find elements using XPath', async()=>{
+    it('should be able to find elements using XPath', async()=>{
         //Navigate to Alert Dialogs
         await $('~App').click()
 
@@ -35,5 +35,22 @@ describe('Find elements on Android app', ()=>{
 
         // Verify the final text
         await expect(await $('//*[@resource-id="android:id/message"]')).toHaveText('You selected: 0 , Command one')
+    })
+
+    it('should be able to find elements using UIAutomator functions', async()=>{
+        // Navigate to Alert Dialog
+        await $('~App').click()
+
+        // Click on Alert Dialogs using UIAutomator
+        await $('android=new UiSelector().textContains("Alert")').click()
+
+        // Click on List Dialog using UIAutomator
+        await $('android=new UiSelector().resourceId("io.appium.android.apis:id/select_button")').click()
+
+        // Click on Command two using UIAutomator
+        await $('android=new UiSelector().text("Command two")').click()
+
+        // Assertion on final text using UIAutomator
+        await expect(await $('android=new UiSelector().text("You selected: 1 , Command two")')).toBeExisting()
     })
 })
