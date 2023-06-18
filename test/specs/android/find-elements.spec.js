685ef70055843227200cb5bbfbf00fb68d70124a
Gagan Keshav <gagankeshav@gmail.com>
2023-06-18 18:35:49 +1200

    Adding test to find elements via XPaths

--- test/specs/android/find-elements.spec.js
+++ test/specs/android/find-elements.spec.js
@@ -19,4 +19,21 @@ describe('Find elements on Android app', ()=>{
         // Assertion
         await expect(clsElement).toHaveText('API Demos')
     })
+
+    it.only('should be able to find elements using XPath', async()=>{
+        //Navigate to Alert Dialogs
+        await $('~App').click()
+
+        // Click on Alert Dialogs using XPath
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+
+        // Click on List Dialog button
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+
+        // Click on Command one
+        await $('//*[@text="Command one"]').click()
+
+        // Verify the final text
+        await expect(await $('//*[@resource-id="android:id/message"]')).toHaveText('You selected: 0 , Command one')
+    })
 })
