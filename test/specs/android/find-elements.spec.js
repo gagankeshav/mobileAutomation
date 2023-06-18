7b76057da4041a548933cd4ac0cbcfe707128111
Gagan Keshav <gagankeshav@gmail.com>
2023-06-18 18:19:38 +1200

    Adding test to find single element via class name

--- test/specs/android/find-elements.spec.js
+++ test/specs/android/find-elements.spec.js
@@ -1,14 +1,22 @@
 describe('Find elements on Android app', ()=>{
 
     it('should be able to find elements using Accessibility ID', async ()=>{
-        //Find the element on UI
+        // Find the element on UI
         const app = await $('~App')
 
-        //Click on the above element
+        // Click on the above element
         await app.click()
 
-        //Assertion
+        // Assertion
         const actionBar = await $('~Action Bar')
-        await expect(actionBar).toBeExisting();
+        await expect(actionBar).toBeExisting()
+    })
+
+    it('should be able to find elements using Class', async()=>{
+        // Find element
+        const clsElement = await $('android.widget.TextView')
+
+        // Assertion
+        await expect(clsElement).toHaveText('API Demos')
     })
 })
