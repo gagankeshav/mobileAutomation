059db62c726c63dd07c80d7b3c9222beba0969a1
Gagan Keshav <gagankeshav@gmail.com>
2023-06-20 23:29:10 +1200

    Adding documentation and committing changed to the package.json file

--- test/specs/ios/find-elements.spec.js
+++ test/specs/ios/find-elements.spec.js
@@ -21,21 +21,24 @@ describe('Find elements on iOS app', ()=>{
         // Find element
         const clsElement = await $$('XCUIElementTypeStaticText')
 
+        // Create an empty list to capture all the elements
         const list = []
 
+        // Iterate through the elements to get their text and push to the above list
         for (const ele of clsElement) {
             list.push(ele.getText())
         }
 
+        // Log the above list
         console.log(list)
     })
 
     it ('should be able to find elements using XPath', async()=>{
+        // Open the Simple alert box
         await $('//XCUIElementTypeStaticText[@value="Alert Views"]').click()
-
         await $('//XCUIElementTypeStaticText[@name="Simple"]').click()
 
-        // Assertion
+        // Assert that the alert box is open
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best")
 
         // Close the alert
@@ -43,13 +46,13 @@ describe('Find elements on iOS app', ()=>{
     })
 
     it ('should be able to find elements using class chain', async()=>{
+        // Open the Simple alert box
         const alertTxt = '**/XCUIElementTypeStaticText[`label CONTAINS "Alert"`]'
         await $(`-ios class chain:${alertTxt}`).click()
-
         const simpleAlert = '**/XCUIElementTypeStaticText[`label == "Simple"`]'
         await $(`-ios class chain:${simpleAlert}`).click()
 
-        // Assertion
+        // Assert that the alert box is open
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best")
 
         // Close the alert
@@ -57,29 +60,35 @@ describe('Find elements on iOS app', ()=>{
     })
 
     it ('should be able to find elements using predicate string', async()=>{
+        // Open the Simple alert box
         const alertTxt = 'value BEGINSWITH[c] "alert"'
         await $(`-ios predicate string:${alertTxt}`).click()
-
         const simpleAlert = 'label == "Simple"'
         await $(`-ios predicate string:${simpleAlert}`).click()
 
-        // Assertion
+        // Assert that the alert box is open
         await expect(await driver.getAlertText()).toContain("A Short Title Is Best")
 
         // Close the alert
         await driver.acceptAlert()
     })
 
-    it.only ('Search Input Field Exercise', async ()=>{
+    it ('Search Input Field Exercise', async ()=>{
+        // Open the default search
         await $('~Search').click()
         await $('~Default').click()
 
+        // Add a value to the default search
         const searchField = 'type == "XCUIElementTypeSearchField"'
         await $(`-ios predicate string:${searchField}`).addValue("New Zealand")
+
+        // Assert that the value has been added to the search box
         await expect($(`-ios predicate string:${searchField}`)).toHaveAttr("value")
 
+        // Clear the text of the search box
         await $('~Clear text').click()
 
+        // Assert that the search box has been cleared of any value
         await expect($(`-ios predicate string:${searchField}`)).not.toHaveAttr("value")
     })
 })
